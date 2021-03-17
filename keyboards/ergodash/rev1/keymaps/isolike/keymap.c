afd500164bca55751f1959d9647383c4eb2b28df
Thomas Spurden <thomas@spurden.name>
2021-03-17 18:15:20 +0000

    Make left and right raise/lower separate
    Hopefully this will reduce the amount of times I am typing a symbol
    followed by a letter on the left hand and annoyingly don't release the
    raise key on the right in time and move the cursor or press page up.

--- keyboards/ergodash/rev1/keymaps/isolike/keymap.c
+++ keyboards/ergodash/rev1/keymaps/isolike/keymap.c
@@ -4,28 +4,38 @@ extern keymap_config_t keymap_config;
 
 #define _QWERTY 0
 #define _RAISE 1
+#define _LOWER 2
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   RAISE,
+  LOWER
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_3key_1us(
      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,          KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LSFT,          KC_RSFT, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
-     KC_NUBS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LSFT,          KC_RSFT, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_WH_U,          KC_RSFT, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+     KC_NUBS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_WH_D,          KC_RSFT, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ENT,           KC_ENT,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NUHS,
-     KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL,       MO(_RAISE), KC_LSFT,          KC_SPC,  MO(_RAISE),       KC_RCTL, KC_RALT, KC_APP,  KC_RSFT
+     KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL,       MO(_RAISE), KC_LSFT,          KC_SPC,  MO(_LOWER),       KC_RCTL, KC_RALT, KC_APP,  KC_RSFT
   ),
 
   [_RAISE] = LAYOUT_3key_1us(
      KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,          _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-     KC_INS,  KC_HOME, KC_UP,   KC_END,  _______, KC_PGUP, _______,          _______, KC_WH_U, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_INS,
-     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_PGDN, _______,          _______, KC_WH_D, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
-     _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, _______,          _______, _______, _______, _______, _______, _______, KC_GRV,
-     _______, KC_RGUI, _______, _______,          _______, _______,          _______, _______,          _______, _______, _______, _______
+     KC_INS,  KC_HOME, KC_UP,   KC_END,  _______, KC_PGUP, _______,          _______, _______, _______, _______, _______, _______, _______,
+     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_PGDN, _______,          _______, _______, _______, _______, _______, _______, _______,
+     _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
+     KC_APP,  KC_RGUI, KC_RALT, KC_RCTL,          _______, _______,          _______, _______,          _______, _______, _______, _______
+  ),
+
+  [_LOWER] = LAYOUT_3key_1us(
+     KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,          _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+     _______, _______, _______, _______, _______, _______, _______,          _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______, _______,
+     _______, _______, _______, _______, _______, _______, _______,          _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______, _______,
+     _______, _______, _______, _______, _______, _______, _______,          _______, KC_QUOT, KC_NUHS, KC_LPRN, KC_RPRN, _______, KC_GRV,
+     _______, _______, _______, _______,          _______, _______,          _______, _______,          _______, _______, _______, _______
   )
 };
 
